9177a6314185f3da543ae4196651a23b6ba9b8db
Juan Gabriel Ramírez Villegas <jgramirezv@gmail.com>
2025-11-30 21:52:25 -0600

    Agregar página de inicio con botón de redirección  a la página portal.

--- calculator/urls.py
+++ calculator/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.portal_view, name='portal'),
-    path('home/', views.portal_view, name='home'),
+    path('', views.home_view, name='home'),
+    path('portal/', views.portal_view, name='portal'),
     path('calculator/', views.calculator_view, name='calculator'),
     path('clear-history/', views.clear_history, name='clear_history'),
     path('log-table/', views.show_log_table, name='log_table'),
